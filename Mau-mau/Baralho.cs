a1adb0785b8d5eb3bd7691dedf44327167030b70
Lucas Souza <lucas.souza@inventsoftware.com.br>
2022-05-13 13:37:55 -0300

    comecei a fazer o maumau

---
+++ Mau-mau/Baralho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mau_mau
+{
+    class Baralho
+    {
+        public string carta { get; set; }
+        public string naipe { get; set; }
+    }
+}
---
+++ Mau-mau/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mau_mau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, jogador;
+
+            Jogador maos = new Jogador();
+            Baralho baralho = new Baralho();
+
+            Queue<string> fila = new Queue<string>();
+
+            try
+            {
+                StreamWriter x;
+
+                string Caminho = @"C:\\arquivoDoMau_mau.txt";
+
+                x = File.AppendText(Caminho);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+
+            Console.WriteLine("Define a quantide de jogadores");
+            jogador = int.Parse(Console.ReadLine());
+
+            if (jogador < 2 || jogador > 10)
+            {
+                Console.WriteLine("Quantidade de jogadores invalida");
+            }
+            else
+            {
+                for (int i = 0; i < jogador; i++)
+                {
+                    fila.Enqueue(i.ToString());
+                }
+
+                Console.Write("fila " + fila.Count());
+            }
+
+
+
+
+
+        }
+    }
+}
