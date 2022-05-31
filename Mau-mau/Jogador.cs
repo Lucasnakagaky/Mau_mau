f8092dd2cf132679ec4b732425647cfe239410ed
Lucas Souza <lucas.souza@inventsoftware.com.br>
2022-05-31 11:42:38 -0300

    funcno

--- Mau-mau/Jogador.cs
+++ Mau-mau/Jogador.cs
@@ -7,12 +7,10 @@ namespace Mau_mau
         public Jogador()
         {
             Mao = new List<string>();
-            Jogadores = new Queue<string>();
             string Nome;
         }
 
         public List<string> Mao { get; set; }
-        public Queue<string> Jogadores { get; set; }
         public string Nome { get; set; }
     }
 }
--- Mau-mau/Program.cs
+++ Mau-mau/Program.cs
@@ -14,8 +14,23 @@ namespace Mau_mau
             MetodosDaCarta _metodosDaCarta = new MetodosDaCarta();
 
 
+            List<string> nomes = new List<string>();
+            Queue<string> Jogadores = new Queue<string>();
             List<string> dados = new List<string>();
             Stack<string> lixo = new Stack<string>();
+
+            nomes.Add("Lucas");
+            nomes.Add("Adriano");
+            nomes.Add("Ferds");
+            nomes.Add("Iago");
+            nomes.Add("Gregorio");
+            nomes.Add("Edvan Haley");
+            nomes.Add("Herlan");
+            nomes.Add("Cabral");
+            nomes.Add("Soares me ajuda");
+            nomes.Add("Eduardo Bolsonaro");
+
+            Console.WriteLine(nomes);
 
             using (StreamReader reader = new StreamReader(@"C:\Users\Invent032\\Desktop\\arquivoDoMau_mau.txt"))
             {
@@ -37,10 +52,18 @@ namespace Mau_mau
             {
                 for (int i = 0; i < int.Parse(dados[1]); i++)
                 {
-                    _jogador.Jogadores.Enqueue(i.ToString());
+                    Console.WriteLine(i);
+
+                    if (i = nomes)
+                    {
+
+                        Jogadores.Enqueue(i.ToString());
+                    }
+
                 }
-                Console.Write("fila de jogadores " + _jogador.Jogadores.Count());
-                Console.Write("=============================");
+                Console.WriteLine("fila de jogadores ");
+                Console.WriteLine(Jogadores.Count());
+                Console.Write("===========================");
             }
 
             var concatenar = dados[2] + (" ") + dados[3] + (" ") + dados[4];
@@ -49,10 +72,31 @@ namespace Mau_mau
 
             var baralho = _metodosDaCarta.Baralho(stringDeCartas);
 
-            Console.WriteLine(baralho);
+
 
             // Agora é pegar cada jogador e acrescentar a mão de cartas dele.
 
+
+            var jogador1 = Jogadores.ToArray()[0];
+            Console.WriteLine(jogador1);
+
+            IDictionary<string, List<string>> dic1 = new Dictionary<string, List<string>>();
+
+
+            foreach (var jogad in Jogadores.ToArray())
+            {
+
+                Console.WriteLine(jogad);
+
+                while (_jogador.Mao.Count() < 5)
+                {
+                    var carta = baralho.Pop();
+                    _jogador.Mao.Add(carta);
+                }
+
+                dic1.Add(jogad, _jogador.Mao);
+            }
+            Console.WriteLine(dic1.Count);
 
         }
     }
